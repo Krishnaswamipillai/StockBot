056242c4f1c1c01dac25c4edb0b176c6ca4ff0f8
azbxx <>
2018-05-17 13:22:08 -0700

    more 20 year

--- api/localstockhandler.py
+++ api/localstockhandler.py
@@ -2,17 +2,17 @@ import os
 from datetime import datetime
 
 months = {
-    'Jan' : 1,'Feb' : 2,'Mar' : 3,'Apr' : 4,'May' : 5,'Jun' : 6,'Jul' : 7,
-    'Aug' : 8,'Sep' : 9, 'Oct' : 10,'Nov' : 11,'Dec' : 12
-    }
+    'Jan' : 1,'Feb' : 2,'Mar' : 3,'Apr' : 4,'May' : 5,'Jun' : 6,
+    'Jul' : 7,'Aug' : 8,'Sep' : 9, 'Oct' : 10,'Nov' : 11,'Dec' : 12}
 
 def convertMonth(month):
     return months[month]
 
 stocks = {}
 days = set()
+directorylist = os.listdir('results8/')
 def loadStocks():
-    for i in os.listdir('results8/'):
+    for i in directorylist:
         print(i)
         loaded = open("results8/" + i, "r").read()
         stocks[i] = eval(loaded)
@@ -21,4 +21,4 @@ def loadStocks():
         d = [x[1].split(" ")[-1] + " " + str(convertMonth(x[1].split(" ")[0])) + " " + x[1].split(" ")[1][:-1] for x in stocks[i]['prices']]
         days.update(d)
 
-    return sorted(days, key=lambda date: datetime.strptime(date, "%Y %m %d"))
+    return dict.fromkeys(days, key=lambda date: datetime.strptime(date, "%Y %m %d"), dict.fromkeys(directorylist, None))

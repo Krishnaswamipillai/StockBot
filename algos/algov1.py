d1de09d8faf1f623a448d349505057c58339f7bc
Krishnaswamipillai <36609892+Krishnaswamipillai@users.noreply.github.com>
2018-05-17 10:24:57 -0700

    Update algov1.py

--- algos/algov1.py
+++ algos/algov1.py
@@ -8,19 +8,23 @@ weight10 =
 #We need to import the 90,30,10 day lists every day. 
 avg90 = np.array(lnrg90)
 avg30 = np.array(lnrg30)
-avg10 = np.array(lnrg10)
+avg10= np.array(lnrg10)
+r290 = np.array(ofLnrg90)
+r230 = np.array(ofLnrg30)
+r210 = np.array(ofLnrg10)
 
 #transaction fees
-tFee = []
+#tFee (InteractiveBrokers- FeePerShare)
+tFeePerShare = 4.95
 weightTFee =
 
 #relative percentage growth as compared to S&P i.e 95%/115% based off of 100% so 15% decrease is equivalent to 85%
 relGrowth = []
 weightRelGrowth =
 
-def stockAvg():
-	return numpy.multiply(avg90,weight90) + numpy.multiply(avg30,weight30) numpy.multiply(avg10,weight10)
+def zCalc():
+	return numpy.multiply((1/(weight90+weight30+weight10)),weightAvg,numpy.add(numpy.multiply(avg90,weight90),numpy.multiply(avg30,weight30),numpy.multiply(avg10,weight10)))
 
 def sigmoid(z):
-	# Z = weightAvg*stockAvg - TFee*weightTfee/stockPrice
+	# Z = weightAvg*stockAvg - TFeePerShare*weightTfee/stockPrice
 	return 1/(1+exp(-z))

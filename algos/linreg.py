5c36c21f9580b2af21aaffe26ca26720352dc2ef
azbxx <>
2018-05-17 09:57:58 -0700

    modified linreg and added more stocks

--- algos/linreg.py
+++ algos/linreg.py
@@ -1,20 +1,18 @@
 from scipy import stats
 
 def LinReg(evaldata):
+#    start = timeit.default_timer()
+    arrayOfStockPrices = (evaldata[ticker]['prices'])
+
     xAxis=[1]
     for x in range (len(arrayOfStockPrices)-1):
         xAxis.append((x+2))
 
-    #start = timeit.default_timer()
-
-    for x in range (1000):
+    '''for x in range (1000):
         slope, intercept, r_value, p_value, std_err = stats.linregress(xAxis,arrayOfStockPrices)
-        print((x+1), "slope:", slope, "  R-squared: ", r_value**2)
+        print((x+1), "slope:", slope, "  R-squared: ", r_value**2)'''
 
-    """
-    stop = timeit.default_timer()
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xAxis,arrayOfStockPrices)
+#    stop = timeit.default_timer()
 
-    print("start", start, "stop:", stop)
-    print("runtime: ", (stop-start))
-    print ("runtime average:", ((stop-start)/1000))
-    """
+    return(slope, r_value**2)

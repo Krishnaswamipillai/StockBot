38bcb19e81e3dea2ec41cce55364736e1795c124
BryceDevoe <39098473+BryceDevoe@users.noreply.github.com>
2018-05-21 08:35:39 -0700

    Update algov1.py

--- algos/algov1.py
+++ algos/algov1.py
@@ -6,17 +6,18 @@ weight10 =
 
 #average each company in a list
 #We need to import the 90,30,10 day lists every day. 
-avg90 = np.array(lnrg90)
-avg30 = np.array(lnrg30)
-avg10= np.array(lnrg10)
-r290 = np.array(ofLnrg90)
-r230 = np.array(ofLnrg30)
-r210 = np.array(ofLnrg10)
+def setData(lnrg90,lnrg30,lnrg10,ofLnrg90,ofLnrg30,ofLnrg10):
+	avg90 = np.array(lnrg90)
+	avg30 = np.array(lnrg30)
+	avg10= np.array(lnrg10)
+	r290 = np.array(ofLnrg90)
+	r230 = np.array(ofLnrg30)
+	r210 = np.array(ofLnrg10)
 
 #transaction fees
 #tFee (InteractiveBrokers- FeePerShare)
 tFeePerShare = 4.95
-weightTFee =
+weightTFee = 
 
 #relative percentage growth as compared to S&P i.e 95%/115% based off of 100% so 15% decrease is equivalent to 85%
 relGrowth = []

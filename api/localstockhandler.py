a5e7d9c23eb01756aef93de3d8ced1dac83e7bfa
azbxx <>
2018-05-21 09:21:01 -0700

    more stock data

--- api/localstockhandler.py
+++ api/localstockhandler.py
@@ -24,16 +24,21 @@ def loadStocks():
         d = [x[1].split(" ")[-1] + " " + str(convertMonth(x[1].split(" ")[0])) + " " + x[1].split(" ")[1][:-1] for x in stocks[i]['prices']]
         days.update(d)
 
+
+    #move to ordered list
+    #add stock price of beginning of list
     returndict = dict.fromkeys(days, [])
-    print(returndict.keys())
+    print(returndict)
     for stock in stocks.keys():
-        print(stock + "---------------------")
         for price in stocks[stock]['prices']:
             x = price[1]
-            print(price[0])
             splitted = x.split(" ")
             date = splitted[-1] + " " + str(convertMonth(splitted[0])) + " " + splitted[1][:-1]
-            print(date)
             returndict[date].append({stock: price[0], "lin30" : None, "lin60" : None, "lin90" : None, "percentageDif" : None})
 
-    print(returndict)
+    counter = 0
+    for i in returndict.keys():
+        if counter % 20 == 0:
+            #print(returndict[i], counter)
+            pass
+        counter += 1

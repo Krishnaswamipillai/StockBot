dea754b0a171ab453c60bb5a1eaa0860791bc58d
azbxx <>
2018-05-22 13:31:06 -0700

    more 20years added, problems with localstockhandler still

--- api/localstockhandler.py
+++ api/localstockhandler.py
@@ -21,13 +21,15 @@ def loadStocks():
         stocks[i] = eval(loaded)
         stockdays = []
 
-        d = [x[1].split(" ")[-1] + " " + str(convertMonth(x[1].split(" ")[0])) + " " + x[1].split(" ")[1][:-1] for x in stocks[i]['prices']]
+        d = []
+        for x in stocks[i]['prices']:
+            d.append(x[1].split(" ")[-1] + " " + str(convertMonth(x[1].split(" ")[0])) + " " + x[1].split(" ")[1][:-1])
+
         days.update(d)
 
 
     #move to ordered list
     #add stock price of beginning of list
-    counter = 0
     returndict = dict.fromkeys(days, {})
     for stock in stocks.keys():
         for price in stocks[stock]['prices']:
@@ -36,14 +38,23 @@ def loadStocks():
             x = price[1]
             splitted = x.split(" ")
             date = splitted[-1] + " " + str(convertMonth(splitted[0])) + " " + splitted[1][:-1]
-            if counter % 100 == 0:
+            if stock == "AMD":
                 print(date)
-            counter += 1
             date = date.strip()
-            returndict[date][stock] = {"price":price[0], "lin30" : None, "lin60" : None, "lin90" : None, "percentageDif" : None}
+            if ("2010 1" in date) and (stock == "AMD"):
+                returndict[date][stock] = {"price":price[0], "lin30" : None, "lin60" : None, "lin90" : None, "percentageDif" : None}
+                print(returndict[date][stock])
+            else:
+                returndict[date][stock] = {"price":price[0], "lin30" : None, "lin60" : None, "lin90" : None, "percentageDif" : None}
     dictToList = []
+    counter = 0
     for i in returndict.keys():
+        counter += 1
         dictToList.append((i, returndict[i]))
+        if counter % 100 == 0:
+            print((i, returndict[i]))
+            print("\n\n\n\n")
+
     returnlist = sorted(dictToList, key=lambda date: datetime.strptime(date[0], "%Y %m %d"))
 
     return returnlist

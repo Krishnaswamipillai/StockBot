ea8d15422eac2166733d5d53cb5b3e71ef7db622
azbxx <>
2018-05-24 10:00:39 -0700

    modified things

--- api/simulation.py
+++ api/simulation.py
@@ -10,7 +10,8 @@ months = {
 def convertMonth(month):
     return months[month]
 
-def getLinArrays(index, arr):
+def getLinArrays(arr):
+    index = len(arr)-1
     arr30 = []
     arr60 = []
     arr90 = []
@@ -30,15 +31,16 @@ def getLinArrays(index, arr):
     return LinReg(arr30), LinReg(arr60), LinReg(arr90)
 
 
+STOCKDIR = 'results8/'
 
 def loadStocks():
     stocks = {}
     days = set()
-    directorylist = os.listdir('results8/')
+    directorylist = os.listdir(STOCKDIR)
 
     for i in directorylist:
         print(i)
-        loaded = open("results8/" + i, "r").read()
+        loaded = open(STOCKDIR + i, "r").read()
         stocks[i] = eval(loaded)
         stockdays = []
 
@@ -54,7 +56,6 @@ def loadStocks():
         returndict[i] = {}
 
     for stock in stocks.keys():
-        print(stock)
         for price in stocks[stock]['prices']:
             x = price[1]
             splitted = x.split(" ")
@@ -71,12 +72,59 @@ def loadStocks():
 
     return returnlist
 
-def processStocks():
-    daystocks = loadStocks()
-
-    counter = 1
-    for x in range(len(daystocks)):
-        min = x-90
-        if min < 0:
-            min = 0
+def process(daystocks, filelist, simday):
+    x = simday
+    print(x)
+    min = x-90
+    if min < 0:
+        min = 0
+    stocks = {}
+    daystolinreg = None
+    if x + 1 <= len(daystocks):
         daystolinreg = daystocks[min: x]
+    else:
+        daystolinreg = daystocks[min:]
+    for i in filelist:
+        stocks[i] = []
+    for day in range(len(daystolinreg)-1):
+        for stock in daystolinreg[day][1].keys():
+            stocks[stock].append(daystolinreg[day][1][stock]['price'])
+    for i in stocks.keys():
+        try:
+            thirty, sixty, ninty = getLinArrays(stocks[i])
+            daystocks[x][1][i]['lin30'] = thirty
+            daystocks[x][1][i]['lin60'] = sixty
+            daystocks[x][1][i]['lin90'] = ninty
+
+        except:
+            pass
+
+    return daystocks
+
+def startSimulation(date1, date2):
+    filelist = os.listdir(STOCKDIR)
+
+    print("""
+    ##########################
+    ### STOCK TRADING BOT ####
+    ##########################
+    """)
+    print("starting.")
+    print("------------------")
+    print("READING STOCK DATA")
+    print("------------------")
+    daystocks = loadStocks()
+    print("------------------")
+    print("DONE LOADING STOCK")
+    print("------------------")
+
+    firstIndex = None
+    endIndex = None
+
+    for i in range(len(daystocks)-1):
+        if daystocks[i][0] == date1:
+            firstIndex = i
+        elif daystocks[i][0] == date2:
+            endIndex = i
+    daystocks = daystocks[firstIndex:endIndex + 1]
+    process()

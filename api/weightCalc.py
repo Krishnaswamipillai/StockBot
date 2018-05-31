38ff758b14c5a41c2f469422322a55f3eac0fe9a
BryceDevoe <39098473+BryceDevoe@users.noreply.github.com>
2018-05-31 14:34:02 -0700

    Update weightCalc.py

--- api/weightCalc.py
+++ api/weightCalc.py
@@ -1,33 +1,34 @@
 day1 = '1 1 2000'
 day2 = '1 1 2018'
 import simulation as s
-w90,w30,w10,avgC,nSig,pSig = 5,10,8,q,2.5,0.5 
+w90,w30,w10,AvgW,nSig,pSig = 5,10,8,.1,2.5,0.5 
 
-def weight90C(w90,w30,w10,avgC,nSig,pSig):
+def weightStep(a,w90,w30,w10,AvgW,nSig,pSig):
   #Dont worry about this function, it should be fine for w90 right now
-  a = 1
+  if a == none:
+    a = 1
   
   for sets in range(0,10):
     s,e = randDay(date1,day2)
     
-    moneyW90P += startSimulation(s, e, initalprice = 100000,w90+a,w30,w10,avgC,nSig,pSig)
-    moneyW90N += startSimulation(s, e, initalprice = 100000,w90-a,w30,w10,avgC,nSig,pSig)
+    moneyW90P += startSimulation(s, e, initalprice = 100000,w90+a,w30,w10,AvgW,nSig,pSig)
+    moneyW90N += startSimulation(s, e, initalprice = 100000,w90-a,w30,w10,AvgW,nSig,pSig)
     
-    moneyW30P += startSimulation(s, e, initalprice = 100000,w90,w30+a,w10,avgC,nSig,pSig)
-    moneyW30N += startSimulation(s, e, initalprice = 100000,w90,w30-a,w10,avgC,nSig,pSig)
+    moneyW30P += startSimulation(s, e, initalprice = 100000,w90,w30+a,w10,AvgW,nSig,pSig)
+    moneyW30N += startSimulation(s, e, initalprice = 100000,w90,w30-a,w10,AvgW,nSig,pSig)
     
-    moneyW10P += startSimulation(s, e, initalprice = 100000,w90,w30,w10+a,avgC,nSig,pSig)
-    moneyW10N += startSimulation(s, e, initalprice = 100000,w90,w30,w10-a,avgC,nSig,pSig)
+    moneyW10P += startSimulation(s, e, initalprice = 100000,w90,w30,w10+a,AvgW,nSig,pSig)
+    moneyW10N += startSimulation(s, e, initalprice = 100000,w90,w30,w10-a,AvgW,nSig,pSig)
     
-    moneyAvgCP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC+a,nSig,pSig)
-    moneyAvgCN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC-a,nSig,pSig)
+    moneyAvgWP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW+a,nSig,pSig)
+    moneyAvgWN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW-a,nSig,pSig)
     
-    moneyNSigP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC,nSig+a,pSig)
-    moneyNSigN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC,nSig-a,pSig)
+    moneyNSigP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW,nSig+a,pSig)
+    moneyNSigN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW,nSig-a,pSig)
     
-    moneyPSigP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC,nSig,pSig+a)
-    moneyPsigN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,avgC,nSig,pSig-a)
-   
+    moneyPSigP += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW,nSig,pSig+a)
+    moneyPsigN += startSimulation(s, e, initalprice = 100000,w90,w30,w10,AvgW,nSig,pSig-a)
+  
   if moneyW90P > moneyW90N and moneyW90P/10 > money:
     w90 = w90+a
   if moneyW90P < moneyW90N and moneyW90N/10 > money:
@@ -36,14 +37,24 @@ def weight90C(w90,w30,w10,avgC,nSig,pSig):
     w30 = w30+a
   if moneyW30P < moneyW30N and moneyW30N/10 > money:
     w30 = w30-a    
-  if moneyW1P > moneyW10N and moneyW10P/10 > money:
+  if moneyW10P > moneyW10N and moneyW10P/10 > money:
     w10 = w10+a
   if moneyW10P < moneyW10N and moneyW10N/10 > money:
     w10 = w10-a  
-  if moneyW90P > moneyW90N and moneyW90P/10 > money:
-    w10 = w10+a
-  if moneyW90P < moneyW90N and moneyW90N/10 > money:
-    w10 = w10-a  
+    
+  if moneyAvgWP > moneyAvgWN and moneyAvgWP/10 > money:
+    avgW = avgW+a
+  if moneyAvgWP < moneyAvgWN and moneyAvgWN/10 > money:
+    avgW = avgW-a  
+  if moneyNSigP > moneyNSigN and moneyNSigP/10 > money:
+    nSig = nSig+a
+  if moneyNSigP < moneyNSigN and moneyNSigN/10 > money:
+    nSig = nSig-a 
+  if moneyPSigP > moneyPSigN and moneyPSigP/10 > money:
+    pSig = pSig+a
+  if moneyPSigP < moneyPSigN and moneyPSigN/10 > money:
+    pSig = pSig-a 
+  
     
       
       
@@ -62,3 +73,4 @@ def randDay(day1,day2):
 
 
     
+
